fcdae942d99c39499a7c5efe819f43dbda0ef7e1
RaidenShogun508 <admin@nguyenhieu.click>
2023-10-29 08:24:21 +0700

    fix error install file

--- install.bat
+++ install.bat
@@ -9,9 +9,11 @@ echo    ##     ##  ##   ##  ##     ##     ##  ##   ##       ##         ##     ##
 echo   ####    ##  ##    ####      ##     ##  ##   ######   ######    ####    ##  ##    ####   
 winget install --id=Python.Python.3.11 -e 
 winget install --id=Git.Git -e 
+curl -o run.bat https://raw.githubusercontent.com/RaidenShogun503/SocialDownloader/main/run.bat
 git clone https://github.com/RaidenShogun503/SocialDownloader
 cd SocialDownloader
 pip install -r requirements.txt
+curl -o run.bat https://raw.githubusercontent.com/RaidenShogun503/SocialDownloader/main/run.bat
 echo  ####      ####    ##  ##   ######  
 echo ## ##    ##  ##   ### ##   ##      
 echo ##  ##   ##  ##   ######   ##      
